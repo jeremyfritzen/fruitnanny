e00c4bf1c9dd25ed5f22d85c1dfc6b8fe49bc639
jeremyfritzen <jeremymolla@gmail.com>
2019-12-08 20:57:38 +0100

    notif

--- docker/notification/temperature.py
+++ docker/notification/temperature.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 import sys
 import Adafruit_DHT
+from pushbullet import Pushbullet
+
+pb =Pushbullet (o.9bUaasge1wTDY2MMnonIjnAY9PunOZYO)
 
 pin = 24
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
 if humidity is not None and temperature is not None:
     print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
+    push = pb.push_note("Test !", "Ca marche !")
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
