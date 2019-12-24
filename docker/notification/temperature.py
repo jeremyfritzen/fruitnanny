9ae5cb8e4d827a06f6945a01d7f6eee9d53ee578
jeremyfritzen <jeremymolla@gmail.com>
2019-12-24 16:00:14 +0100

    update temperature.py

--- docker/notification/temperature.py
+++ docker/notification/temperature.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
-import sys
-from board import 24
+import time
+import adafruit_dht
+import board
 from pushbullet import Pushbullet
 
-Pushbullet_API_key = ""
-pin = 24
+dhtDevice = adafruit_dht.DHT22(board.D24)
+pb = Pushbullet("")
 
-dht_device = adafruit_dht.DHT22(pin)
-pb = Pushbullet(Pushbullet_API_key)
 
-temperature = dht_device.temperature
-humidity = dht_device.humidity
+while True:
+	try:
+		# Print the values to the serial port
+		temperature_c = dhtDevice.temperature
+		message = ("La température est de {}".format(temperature_c))
+		if temperature_c > 21 or temperature_c < 18 :
+			push = pb.push_note("Rpi-Nanny", message)
+			print(message)
+	except RuntimeError as error:
+		# Errors happen fairly often, DHT's are hard to read, just keep going
+		print(error.args[0])
+
+	time.sleep(5.0)
+
+# Print the values to the serial port
+#print(message)
